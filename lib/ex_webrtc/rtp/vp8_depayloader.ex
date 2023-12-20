60a828c6722206b9b4c8b031314d62b15ca58de5
Michał Śledź <michalsledz34@gmail.com>
2023-12-20 21:18:52 +0100

    Add VP8Depayloader

---
+++ lib/ex_webrtc/rtp/vp8_depayloader.ex
@@ -0,0 +1,70 @@
+defmodule ExWebRTC.RTP.VP8Depayloader do
+  @moduledoc """
+  Reassembles VP8 frames from RTP packets.
+  """
+  require Logger
+
+  alias ExWebRTC.RTP.VP8Payload
+
+  @opaque t() :: %__MODULE__{
+            current_frame: nil,
+            current_timestamp: nil
+          }
+
+  defstruct [:current_frame, :current_timestamp]
+
+  @spec new() :: t()
+  def new() do
+    %__MODULE__{}
+  end
+
+  @spec write(t(), ExRTP.Packet.t()) :: {:ok, t()} | {:ok, binary(), t()}
+  def write(depayloader, packet) do
+    with {:ok, vp8_payload} <- VP8Payload.parse(packet.payload) do
+      depayloader =
+        case {depayloader.current_frame, vp8_payload} do
+          {nil, %VP8Payload{s: 1, pid: 0}} ->
+            %{
+              depayloader
+              | current_frame: vp8_payload.payload,
+                current_timestamp: packet.timestamp
+            }
+
+          {nil, _vp8_payload} ->
+            Logger.debug("Dropping vp8 payload as it doesn't start a new frame")
+            depayloader
+
+          {_current_frame, %VP8Payload{s: 1, pid: 0}} ->
+            Logger.debug("""
+            Received packet that starts a new frame without finishing the previous frame. \
+            Droping previous frame.\
+            """)
+
+            %{
+              depayloader
+              | current_frame: vp8_payload.payload,
+                current_timestamp: packet.timestamp
+            }
+
+          _ when packet.timestamp != depayloader.current_timestamp ->
+            Logger.debug("""
+            Received packet with timestamp from a new frame that is not a beginning of this frame \
+            and without finishing the previous frame. Droping both.\
+            """)
+
+            %{depayloader | current_frame: nil, current_timestamp: nil}
+
+          {current_frame, vp8_payload} ->
+            %{depayloader | current_frame: current_frame <> vp8_payload.payload}
+        end
+
+      case {depayloader.current_frame, packet.marker} do
+        {current_frame, true} when current_frame != nil ->
+          {:ok, current_frame, %{depayloader | current_frame: nil, current_timestamp: nil}}
+
+        _ ->
+          {:ok, depayloader}
+      end
+    end
+  end
+end
---
+++ test/ex_webrtc/rtp/vp8_depayloader_test.exs
@@ -0,0 +1,57 @@
+defmodule ExWebRTC.RTP.VP8DepayloaderTest do
+  use ExUnit.Case, async: true
+
+  alias ExWebRTC.RTP.{VP8Payload, VP8Depayloader}
+
+  test "write/2" do
+    depayloader = VP8Depayloader.new()
+    # random vp8 data, not necessarily correct
+    data = <<0, 1, 2, 3>>
+
+    # packet with entire frame
+    vp8_payload = %VP8Payload{n: 0, s: 1, pid: 0, payload: data}
+    vp8_payload = VP8Payload.serialize(vp8_payload)
+
+    packet = ExRTP.Packet.new(vp8_payload, 0, 0, 0, 0, marker: true)
+
+    assert {:ok, ^data, %{current_frame: nil, current_timestamp: nil} = depayloader} =
+             VP8Depayloader.write(depayloader, packet)
+
+    # packet that doesn't start a new frame
+    vp8_payload = %VP8Payload{n: 0, s: 0, pid: 0, payload: data}
+    vp8_payload = VP8Payload.serialize(vp8_payload)
+
+    packet = ExRTP.Packet.new(vp8_payload, 0, 0, 0, 0)
+
+    assert {:ok, %{current_frame: nil, current_timestamp: nil} = depayloader} =
+             VP8Depayloader.write(depayloader, packet)
+
+    # packet that starts a new frame without finishing the previous one
+    vp8_payload = %VP8Payload{n: 0, s: 1, pid: 0, payload: data}
+    vp8_payload = VP8Payload.serialize(vp8_payload)
+
+    packet = ExRTP.Packet.new(vp8_payload, 0, 0, 0, 0)
+
+    assert {:ok, %{current_frame: ^data, current_timestamp: 0} = depayloader} =
+             VP8Depayloader.write(depayloader, packet)
+
+    data2 = data <> <<0>>
+    vp8_payload = %VP8Payload{n: 0, s: 1, pid: 0, payload: data2}
+    vp8_payload = VP8Payload.serialize(vp8_payload)
+
+    packet = ExRTP.Packet.new(vp8_payload, 0, 0, 3000, 0)
+
+    assert {:ok, %{current_frame: ^data2, current_timestamp: 3000} = depayloader} =
+             VP8Depayloader.write(depayloader, packet)
+
+    # packet with timestamp from a new frame that is not a beginning of this frame
+    data2 = data
+    vp8_payload = %VP8Payload{n: 0, s: 0, pid: 0, payload: data2}
+    vp8_payload = VP8Payload.serialize(vp8_payload)
+
+    packet = ExRTP.Packet.new(vp8_payload, 0, 0, 6000, 0)
+
+    assert {:ok, %{current_frame: nil, current_timestamp: nil}} =
+             VP8Depayloader.write(depayloader, packet)
+  end
+end
