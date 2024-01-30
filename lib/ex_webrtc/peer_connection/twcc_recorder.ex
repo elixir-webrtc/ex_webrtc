72d57831f9fbf93242830edf0e34a6cf1fe1943c
Łukasz Wala <lukaszpwala@gmail.com>
2024-01-30 15:40:15 +0100

    Add `TWCCRecorder.get_feedback/1` implementation

--- lib/ex_webrtc/peer_connection/twcc_recorder.ex
+++ lib/ex_webrtc/peer_connection/twcc_recorder.ex
@@ -21,6 +21,7 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
     @spec get_time(t()) :: non_neg_integer()
     def get_time(%__MODULE__{base: base}) do
       # should be always positive thanks to this
+      # TODO: storing such a huge values might be inefficient
       System.monotonic_time(:microsecond) + base
     end
 
@@ -28,18 +29,23 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
     def to_ref(timestamp), do: div(timestamp, 1000 * 64)
 
     @spec get_delta(non_neg_integer(), non_neg_integer()) :: non_neg_integer()
-    def get_delta(ref, timestamp) do
-      # ref as returned from `to_ref/1`
-      # timestamp as returned by `get_time/1`
-      ref_timestamp = ref * 64 * 1000
-      delta = timestamp - ref_timestamp
-      div(delta, 250)
+    def get_delta(prev_ts, cur_ts) do
+      delta = cur_ts - prev_ts
+      # thanks to this delta is rounded to the closest value
+      cond do
+        delta < 0 -> delta - 125
+        true -> delta + 125
+      end
+      |> div(250)
     end
   end
 
   import Bitwise
 
   alias ExRTCP.Packet.TransportFeedback.CC
+
+  @uint8_range 0..255
+  @int16_range -32_768..32_767
 
   @max_seq_num 0xFFFF
   @breakpoint 0x7FFF
@@ -59,7 +65,7 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
   # start, base - inclusive, end - exclusive
   # start, end - actual range where map values might be set
   # base - from where packets should be added to next feedback
-  # if end_seq_no >= start_seq_no, then no packets are available
+  # if end_seq_no == start_seq_no, then no packets are available
   @enforce_keys [:media_ssrc, :sender_ssrc]
   defstruct @enforce_keys ++
               [
@@ -73,7 +79,7 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
 
   @spec record_packet(t(), non_neg_integer()) :: t()
   def record_packet(%{start_seq_no: nil, end_seq_no: nil} = recorder, seq_no) do
-    # initial case
+    # initial case, should only occur once
     timestamp = Timer.get_time(recorder.timer)
 
     %__MODULE__{
@@ -86,8 +92,6 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
   end
 
   def record_packet(recorder, raw_seq_no) do
-    timestamp = Timer.get_time(recorder.timer)
-
     %__MODULE__{
       base_seq_no: base_seq_no,
       start_seq_no: start_seq_no,
@@ -95,17 +99,12 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
       timestamps: timestamps
     } = recorder
 
+    timestamp = Timer.get_time(recorder.timer)
+
+    # internally, we don't wrap the sequence number around 2^16
+    # so when receiving a new seq_num, we have to "unroll" it
     seq_no = unroll(raw_seq_no, end_seq_no)
-
-    # dont overwrite timestamps already present in the map, unless we already
-    # included them in a feedback (maybe we shouldn't at all?)
-    timestamps =
-      if seq_no < base_seq_no do
-        Map.put(timestamps, seq_no, timestamp)
-      else
-        Map.put_new(timestamps, seq_no, timestamp)
-      end
-
+    timestamps = Map.put_new(timestamps, seq_no, timestamp)
     base_seq_no = if seq_no < base_seq_no, do: seq_no, else: base_seq_no
 
     {start_seq_no, end_seq_no} =
@@ -126,8 +125,6 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
   end
 
   defp unroll(seq_no, end_seq_no) do
-    # internally, we dont wrap the sequence number around 2^16
-    # so when receiving a new seq_num, we have to "unroll" it
     end_rolled = roll(end_seq_no)
     delta = seq_no - end_rolled
 
@@ -191,11 +188,6 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
     do: {recorder, Enum.reverse(feedbacks)}
 
   def get_feedback(recorder, feedbacks) do
-    {feedback, recorder} = create_feedback(recorder)
-    get_feedback(recorder, [feedback | feedbacks])
-  end
-
-  def create_feedback(recorder) do
     %__MODULE__{
       sender_ssrc: sender_ssrc,
       media_ssrc: media_ssrc,
@@ -205,24 +197,112 @@ defmodule ExWebRTC.PeerConnection.TWCCRecorder do
       timestamps: timestamps
     } = recorder
 
-    reference_time =
+    base_timestamp =
       base_seq_no..(end_seq_no - 1)
       |> Enum.find_value(&Map.get(timestamps, &1))
-      |> Timer.to_ref()
+
+    # TODO: fix Timer.to_ref (we need to pass valid base_ts to add_packets)
+    ref_timestamp = Timer.to_ref(base_timestamp)
+
+    {chunks, deltas, new_base} =
+      add_packets(timestamps, base_seq_no, end_seq_no, base_timestamp)
 
     feedback = %CC{
-      sender_ssrc: sender_ssrc,
       media_ssrc: media_ssrc,
+      sender_ssrc: sender_ssrc,
       fb_pkt_count: fb_pkt_count,
-      packet_status_count: 0,
-      base_sequence_number: base_seq_no,
-      reference_time: reference_time
+      base_sequence_number: roll(base_seq_no),
+      packet_status_count: new_base - base_seq_no,
+      reference_time: ref_timestamp,
+      packet_chunks: Enum.reverse(chunks) |> Enum.map(&encode_chunk/1),
+      recv_deltas: Enum.reverse(deltas)
     }
 
-    # TODO: add packets
-    new_base = :siema
+    recorder = %__MODULE__{
+      recorder
+      | fb_pkt_count: fb_pkt_count + 1,
+        base_seq_no: new_base
+    }
 
-    recorder = %__MODULE__{recorder | fb_pkt_count: fb_pkt_count + 1, base_seq_no: new_base}
-    {feedback, recorder}
+    # TODO: handle feedbacks with no packets?
+    get_feedback(recorder, [feedback | feedbacks])
+  end
+
+  defp add_packets(timestamps, base_no, end_no, prev_ts, chunks \\ [], deltas \\ [])
+
+  defp add_packets(_timestamps, end_no, end_no, _prev_ts, chunks, deltas) do
+    {chunks, deltas, end_no}
+  end
+
+  defp add_packets(timestamps, base_no, end_no, prev_ts, chunks, deltas) do
+    {delta, prev_ts} =
+      case Map.fetch(timestamps, base_no) do
+        {:ok, ts} -> {Timer.get_delta(prev_ts, ts), ts}
+        :error -> {nil, prev_ts}
+      end
+
+    symbol =
+      cond do
+        is_nil(delta) -> :not_received
+        delta in @uint8_range -> :small_delta
+        delta in @int16_range -> :large_delta
+        true -> nil
+      end
+
+    if is_nil(symbol) do
+      {chunks, deltas, base_no}
+    else
+      chunks = add_to_chunk(symbol, chunks)
+      deltas = [delta | deltas]
+      add_packets(timestamps, base_no + 1, end_no, prev_ts, chunks, deltas)
+    end
+  end
+
+  defp add_to_chunk(symbol, []), do: [new_chunk(symbol)]
+
+  defp add_to_chunk(symbol, [last_chunk | rest_chunks]) do
+    {symbols, large?, mixed?} = last_chunk
+
+    if can_add?(symbols, large?, mixed?, symbol) do
+      last_chunk = {
+        [symbol | symbols],
+        large? or symbol == :large_delta,
+        mixed? or symbol != hd(symbols)
+      }
+
+      [last_chunk | rest_chunks]
+    else
+      new_chunk = new_chunk(symbol)
+      [new_chunk, last_chunk | rest_chunks]
+    end
+  end
+
+  defp new_chunk(symbol), do: {[symbol], symbol == :large_delta, false}
+
+  defp can_add?(symbols, large?, mixed?, symbol) do
+    length(symbols) < 7 or
+      (length(symbols) < 14 and not large? and symbol != :large_delta) or
+      (length(symbols) < 0x1FFF and not mixed? and symbol == hd(symbols))
+  end
+
+  defp encode_chunk({symbols, _large?, false}) do
+    %CC.RunLength{run_length: length(symbols), status_symbol: hd(symbols)}
+  end
+
+  defp encode_chunk({symbols, _large?, _mixed?}) do
+    # only the last packet's length might be different than 7 or 14
+    # so I add padding of :not_received symbols
+    # but the packet count in feedback header does not include the padding
+    # so I assume it's fine and the padding is discarded by chrome?
+    len = length(symbols)
+
+    pad_len =
+      cond do
+        len <= 7 -> 7 - len
+        len <= 14 -> 14 - len
+      end
+
+    symbols = List.duplicate(:not_received, pad_len) ++ symbols
+    %CC.StatusVector{symbols: Enum.reverse(symbols)}
   end
 end
