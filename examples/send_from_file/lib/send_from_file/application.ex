c5cf5bd94b506de88b8d295a08d7a783a0aee782
Łukasz Wala <lukaszpwala@gmail.com>
2024-02-02 13:38:54 +0100

    Rewrite `send_from_file` example

---
+++ examples/send_from_file/lib/send_from_file/application.ex
@@ -0,0 +1,19 @@
+defmodule SendFromFile.Application do
+  use Application
+
+  require Logger
+
+  @ip {127, 0, 0, 1}
+  @port 8829
+
+  @impl true
+  def start(_type, _args) do
+    Logger.configure(level: :info)
+
+    children = [
+      {Bandit, plug: SendFromFile.Router, ip: @ip, port: @port}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
