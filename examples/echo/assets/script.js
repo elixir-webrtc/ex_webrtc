ff6310d9be98f72dba06147d4c7831496b05334c
Łukasz Wala <lukaszpwala@gmail.com>
2024-02-02 11:44:07 +0100

    Rewrite `echo` example

---
+++ examples/echo/assets/script.js
@@ -0,0 +1,45 @@
+const pcConfig = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' },] };
+const mediaConstraints = {video: true, audio: true}
+const address = "ws://127.0.0.1:8829/ws"
+
+const ws = new WebSocket(address);
+ws.onopen = _ => start_connection(ws);
+ws.onclose = event => console.log("WebSocket connection was terminated:", event);
+
+const start_connection = async (ws) => {
+  const videoPlayer = document.getElementById("videoPlayer");
+  videoPlayer.srcObject = new MediaStream();
+
+  const pc = new RTCPeerConnection(pcConfig);
+  pc.ontrack = event => videoPlayer.srcObject.addTrack(event.track);
+  pc.onicecandidate = event => {
+    if (event.candidate === null) return;
+
+    console.log("Sent ICE candidate:", event.candidate);
+    ws.send(JSON.stringify({ type: "ice", data: event.candidate }));
+  };
+
+  const localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+  for (const track of localStream.getTracks()) {
+    pc.addTrack(track, localStream);
+  }
+
+  ws.onmessage = async event => {
+    const {type, data} = JSON.parse(event.data);
+
+    switch (type) {
+      case "answer":
+        console.log("Received SDP answer:", data);
+        await pc.setRemoteDescription(data)
+        break;
+      case "ice":
+        console.log("Recieved ICE candidate:", data);
+        await pc.addIceCandidate(data);
+    }
+  };
+
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  console.log("Sent SDP offer:", offer)
+  ws.send(JSON.stringify({type: "offer", data: offer}));
+};
