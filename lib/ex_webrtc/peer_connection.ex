3dd6824f7274a21f0927824c94d928508c4663cb
Andrew <andrewianstewart@gmail.com>
2024-02-17 07:17:22 -0500

    :send_rtp handle_cast receives track instead of id

--- lib/ex_webrtc/peer_connection.ex
+++ lib/ex_webrtc/peer_connection.ex
@@ -715,7 +715,7 @@ defmodule ExWebRTC.PeerConnection do
   end
 
   @impl true
-  def handle_cast({:send_rtp, track_id, packet}, state) do
+  def handle_cast({:send_rtp, %MediaStreamTrack{id: track_id}, packet}, state) do
     # TODO: iterating over transceivers is not optimal
     # but this is, most likely, going to be refactored anyways
     {transceiver, idx} =
