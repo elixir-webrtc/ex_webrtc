4398c9951e71fb9f9a448a241a43d279d1401ea2
Łukasz Wala <lukaszpwala@gmail.com>
2024-02-26 12:53:25 +0100

    Add test for `RTPReceiver.ReportRecorder.get_report/2`

--- test/ex_webrtc/rtp_receiver/report_recorder_test.exs
+++ test/ex_webrtc/rtp_receiver/report_recorder_test.exs
@@ -1,7 +1,9 @@
 defmodule ExWebRTC.RTPReceiver.ReportRecorderTest do
   use ExUnit.Case, async: true
 
-  alias ExRTCP.Packet.SenderReport
+  import Bitwise
+
+  alias ExRTCP.Packet.{SenderReport, ReceiverReport, ReceptionReport}
   alias ExRTP.Packet
   alias ExWebRTC.RTPReceiver.ReportRecorder
 
@@ -10,24 +12,22 @@ defmodule ExWebRTC.RTPReceiver.ReportRecorderTest do
   @rtp_ts 234_444
   @packet Packet.new(<<>>, sequence_number: @seq_no, timestamp: @rtp_ts)
   @clock_rate 90_000
+  @sender_report %SenderReport{
+    ssrc: 0,
+    ntp_timestamp: 0xFFFF11111111FFFF,
+    rtp_timestamp: 0,
+    packet_count: 0,
+    octet_count: 0
+  }
   @recorder %ReportRecorder{
     ssrc: 123_456,
     sender_ssrc: 654_321,
     clock_rate: @clock_rate
   }
 
-  @tag :wip
   test "record_report/3" do
-    sr = %SenderReport{
-      ssrc: 0,
-      ntp_timestamp: 0xFFFF11111111FFFF,
-      rtp_timestamp: 0,
-      packet_count: 0,
-      octet_count: 0
-    }
-
     ts = System.monotonic_time()
-    recorder = ReportRecorder.record_report(@recorder, sr, ts)
+    recorder = ReportRecorder.record_report(@recorder, @sender_report, ts)
 
     assert %ReportRecorder{
              last_sr_ntp_timestamp: 0x11111111,
@@ -104,6 +104,9 @@ defmodule ExWebRTC.RTPReceiver.ReportRecorderTest do
       assert actually_lost == lost_packets
     end
 
+    test "packets with wrapping sequence numbers" do
+    end
+
     test "properly calculates jitter" do
       # 20 ms = clock_rate * (20/1000) in RTP timestamp units
       ts_diff = 20
@@ -133,5 +136,54 @@ defmodule ExWebRTC.RTPReceiver.ReportRecorderTest do
 
       assert_in_delta recorder.jitter, 0, 0.5
     end
+  end
+
+  test "get_report/2" do
+    end_seq_no = 65_532
+
+    recorder =
+      65_500..end_seq_no
+      |> Enum.reduce(@recorder, fn i, recorder ->
+        packet = %Packet{@packet | sequence_number: i}
+        ReportRecorder.record_packet(recorder, packet, @rand_ts)
+      end)
+      |> ReportRecorder.record_report(@sender_report, @rand_ts)
+
+    one_second = System.convert_time_unit(1, :second, :native)
+    assert {report, recorder} = ReportRecorder.get_report(recorder, @rand_ts + one_second)
+    assert %ReceiverReport{reports: [rec_report]} = report
+
+    assert %ReceptionReport{
+             delay: 65_536,
+             last_sr: 0x11111111,
+             fraction_lost: 0,
+             last_sequence_number: ^end_seq_no,
+             total_lost: 0
+           } = rec_report
+
+    # now let's drop some and wrap around seq_no boundary
+    end_seq_no = 30
+
+    recorder =
+      Enum.reduce(0..end_seq_no//3, recorder, fn i, recorder ->
+        packet = %Packet{@packet | sequence_number: i}
+        ReportRecorder.record_packet(recorder, packet, @rand_ts)
+      end)
+
+    assert {report, _recorder} = ReportRecorder.get_report(recorder, @rand_ts + 2 * one_second)
+    assert %ReceiverReport{reports: [rec_report]} = report
+
+    lost = 23
+
+    assert %ReceptionReport{
+             delay: 131_072,
+             last_sr: 0x11111111,
+             fraction_lost: fraction_lost,
+             last_sequence_number: last_sequence_number,
+             total_lost: ^lost
+           } = rec_report
+
+    assert last_sequence_number == (1 <<< 16 ||| end_seq_no)
+    assert fraction_lost == round(lost * 256 / 34)
   end
 end
