43da908e96245f3fced4def54675b73d20406707
Michał Śledź <michalsledz34@gmail.com>
2024-03-04 22:27:56 +0100

    Fix SDPUtils.get_dtls_role/1

--- lib/ex_webrtc/sdp_utils.ex
+++ lib/ex_webrtc/sdp_utils.ex
@@ -127,12 +127,16 @@ defmodule ExWebRTC.SDPUtils do
           {:ok, :active | :passive | :actpass}
           | {:error, :missing_dtls_role | :conflicting_dtls_roles}
   def get_dtls_role(sdp) do
-    session_role = ExSDP.get_attribute(sdp, :setup)
+    session_role =
+      case ExSDP.get_attribute(sdp, :setup) do
+        {:setup, setup} -> setup
+        nil -> nil
+      end
 
     mline_roles =
       sdp.media
       |> Enum.flat_map(&ExSDP.get_attributes(&1, :setup))
-      |> Enum.map(fn {_, setup} -> setup end)
+      |> Enum.map(fn {:setup, setup} -> setup end)
 
     case {session_role, mline_roles} do
       {nil, []} ->
