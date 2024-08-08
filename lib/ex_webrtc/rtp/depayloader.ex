9b1c327c051d17a95146f60445ffad7349db8657
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2024-08-08 10:53:11 +0200

    Dynamic dispatch for (de)payloaders

--- lib/ex_webrtc/rtp/depayloader.ex
+++ lib/ex_webrtc/rtp/depayloader.ex
@@ -3,6 +3,8 @@ defmodule ExWebRTC.RTP.Depayloader do
   Behaviour for ExWebRTC Depayloaders.
   """
 
+  alias ExWebRTC.RTPCodecParameters
+
   @type depayloader :: struct()
 
   @doc """
@@ -20,4 +22,33 @@ defmodule ExWebRTC.RTP.Depayloader do
   """
   @callback depayload(depayloader(), packet :: ExRTP.Packet.t()) ::
               {binary() | nil, depayloader()}
+
+  @doc """
+  TODO WRITEME
+  """
+  @spec new(RTPCodecParameters.t(), any()) ::
+          {:ok, depayloader()} | {:error, :no_depayloader_for_codec}
+  def new(codec_params, options \\ nil) do
+    with {:ok, module} <- match_depayloader_module(codec_params.mime_type) do
+      depayloader = if is_nil(options), do: module.new(), else: module.new(options)
+
+      {:ok, depayloader}
+    end
+  end
+
+  @doc """
+  TODO WRITEME
+  """
+  @spec depayload(depayloader(), ExRTP.Packet.t()) :: {binary() | nil, depayloader()}
+  def depayload(%module{} = depayloader, packet) do
+    module.depayload(depayloader, packet)
+  end
+
+  defp match_depayloader_module(mime_type) do
+    case String.downcase(mime_type) do
+      "video/vp8" -> {:ok, ExWebRTC.RTP.VP8.Depayloader}
+      "audio/opus" -> {:ok, ExWebRTC.RTP.Opus.Depayloader}
+      _other -> {:error, :no_depayloader_for_codec}
+    end
+  end
 end
--- lib/ex_webrtc/rtp/payloader.ex
+++ lib/ex_webrtc/rtp/payloader.ex
@@ -3,6 +3,8 @@ defmodule ExWebRTC.RTP.Payloader do
   Behaviour for ExWebRTC Payloaders.
   """
 
+  alias ExWebRTC.RTPCodecParameters
+
   @type payloader :: struct()
 
   @doc """
@@ -18,4 +20,33 @@ defmodule ExWebRTC.RTP.Payloader do
   Returns the packets together with the updated payloader struct.
   """
   @callback payload(payloader(), frame :: binary()) :: {[ExRTP.Packet.t()], payloader()}
+
+  @doc """
+  TODO WRITEME
+  """
+  @spec new(RTPCodecParameters.t(), any()) ::
+          {:ok, payloader()} | {:error, :no_payloader_for_codec}
+  def new(codec_params, options \\ nil) do
+    with {:ok, module} <- match_payloader_module(codec_params.mime_type) do
+      payloader = if is_nil(options), do: module.new(), else: module.new(options)
+
+      {:ok, payloader}
+    end
+  end
+
+  @doc """
+  TODO WRITEME
+  """
+  @spec payload(payloader(), binary()) :: {[ExRTP.Packet.t()], payloader()}
+  def payload(%module{} = payloader, frame) do
+    module.payload(payloader, frame)
+  end
+
+  defp match_payloader_module(mime_type) do
+    case String.downcase(mime_type) do
+      "video/vp8" -> {:ok, ExWebRTC.RTP.VP8.Payloader}
+      "audio/opus" -> {:ok, ExWebRTC.RTP.Opus.Payloader}
+      _other -> {:error, :no_payloader_for_codec}
+    end
+  end
 end
