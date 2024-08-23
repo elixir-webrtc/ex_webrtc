d93b613fb9bb1c210849c78893043eb34d1319de
Łukasz Wala <lukaszpwala@gmail.com>
2024-08-23 16:16:38 +0200

    Remove dependency on `ExUnit` from test helpers

--- test/support/test_utils.ex
+++ test/support/test_utils.ex
@@ -1,32 +1,43 @@
 defmodule ExWebRTC.Support.TestUtils do
   @moduledoc false
 
-  use ExUnit.Case
-
   alias ExWebRTC.PeerConnection
 
   @spec negotiate(PeerConnection.peer_connection(), PeerConnection.peer_connection()) :: :ok
   def negotiate(pc1, pc2) do
-    assert {:ok, offer} = PeerConnection.create_offer(pc1)
-    assert :ok = PeerConnection.set_local_description(pc1, offer)
-    assert :ok = PeerConnection.set_remote_description(pc2, offer)
-    assert {:ok, answer} = PeerConnection.create_answer(pc2)
-    assert :ok = PeerConnection.set_local_description(pc2, answer)
-    assert :ok = PeerConnection.set_remote_description(pc1, answer)
+    {:ok, offer} = PeerConnection.create_offer(pc1)
+    :ok = PeerConnection.set_local_description(pc1, offer)
+    :ok = PeerConnection.set_remote_description(pc2, offer)
+    {:ok, answer} = PeerConnection.create_answer(pc2)
+    :ok = PeerConnection.set_local_description(pc2, answer)
+    :ok = PeerConnection.set_remote_description(pc1, answer)
     :ok
   end
 
   @spec connect(PeerConnection.peer_connection(), PeerConnection.peer_connection()) :: :ok
   def connect(pc1, pc2) do
     # exchange ICE candidates
-    assert_receive {:ex_webrtc, ^pc1, {:ice_candidate, candidate}}
-    :ok = PeerConnection.add_ice_candidate(pc2, candidate)
-    assert_receive {:ex_webrtc, ^pc2, {:ice_candidate, candidate}}
-    :ok = PeerConnection.add_ice_candidate(pc1, candidate)
+    receive do
+      {:ex_webrtc, ^pc1, {:ice_candidate, candidate}} ->
+        :ok = PeerConnection.add_ice_candidate(pc2, candidate)
+    after
+      2000 -> raise "Unable to negotiate"
+    end
 
-    # wait to establish connection
-    assert_receive {:ex_webrtc, ^pc1, {:connection_state_change, :connected}}
-    assert_receive {:ex_webrtc, ^pc2, {:connection_state_change, :connected}}
+    receive do
+      {:ex_webrtc, ^pc2, {:ice_candidate, candidate}} ->
+        :ok = PeerConnection.add_ice_candidate(pc1, candidate)
+    after
+      2000 -> raise "Unable to negotiate"
+    end
+
+    for pc <- [pc1, pc2] do
+      receive do
+        {:ex_webrtc, ^pc, {:connection_state_change, :connected}} -> :ok
+      after
+        2000 -> raise "Unable to connect"
+      end
+    end
 
     :ok
   end
