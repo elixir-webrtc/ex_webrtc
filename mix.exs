3fc9cd0ead080f617eaa942852f9be00e30d8e97
Michał Śledź <michalsledz34@gmail.com>
2024-12-02 17:27:15 +0100

    Update deps. Bump version to 0.7.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExWebRTC.MixProject do
   use Mix.Project
 
-  @version "0.6.3"
+  @version "0.7.0"
   @source_url "https://github.com/elixir-webrtc/ex_webrtc"
 
   def project do
@@ -57,7 +57,7 @@ defmodule ExWebRTC.MixProject do
   defp deps do
     [
       {:ex_sdp, "~> 1.0"},
-      {:ex_ice, "~> 0.8.3"},
+      {:ex_ice, "~> 0.9.0"},
       {:ex_dtls, "~> 0.16.0"},
       {:ex_libsrtp, "~> 0.7.1"},
       {:ex_rtp, "~> 0.4.0"},
