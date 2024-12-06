5d799e028ac9477b37fd0a7aec894d646c0a24e1
Michał Śledź <michalsledz34@gmail.com>
2024-12-06 18:24:10 +0100

    Check H264 FMTP params

--- examples/whip_whep/lib/whip_whep/peer_supervisor.ex
+++ examples/whip_whep/lib/whip_whep/peer_supervisor.ex
@@ -110,7 +110,7 @@ defmodule WhipWhep.PeerSupervisor do
     receive do
       {:ex_webrtc, ^pc, {:ice_gathering_state_change, :complete}} -> :ok
     after
-      1000 -> {:error, :timeout}
+      1000 -> :ok
     end
   end
 
