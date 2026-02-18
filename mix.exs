69b91d74484efa5f9cd010197242fcff54d32856
Bernard Gawor <81643606+Gawor270@users.noreply.github.com>
2026-02-18 15:19:18 +0100

    bump ex_sdp (#245)

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule ExWebRTC.MixProject do
 
   defp deps do
     [
-      {:ex_sdp, "~> 1.0"},
+      {:ex_sdp, "~> 1.1.3"},
       {:ex_ice, github: "elixir-webrtc/ex_ice", ref: "34824b277fcbc88234a2945f68a1304e4d77a212"},
       {:ex_dtls, "~> 0.18.0"},
       {:ex_libsrtp, "~> 0.7.1"},
